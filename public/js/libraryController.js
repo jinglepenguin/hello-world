d48e96c04b00b96903eb5a3f596ef421fc265102
jinglepenguin <Elaine.Chu@rci.rogers.ca>
2016-03-22 09:32:44 -0400

    some more code

--- public/js/libraryController.js
+++ public/js/libraryController.js
@@ -2,24 +2,23 @@ var libraryApp = angular.module('libraryApp', []);
 
 libraryApp.controller("libraryCtrl", LibraryController);
 
-function LibraryController(){
-    this.books = [
-        Book(100, 'Harry Potter', 'Mary', true),
-        Book(200, 'Lord of the Rings', 'John', false),
-        Book(300, 'Legend of Condor Heroes', 'Pater', true),
-        Book(400, 'AngularJS', 'Susie', false)
+function LibraryController($scope){
+    $scope.books = [
+        new Book(100, 'Harry Potter', 'Mary', 'I'),
+        new Book(200, 'Lord of the Rings', 'John', 'O'),
+        new Book(300, 'Legend of Condor Heroes', 'Pater', 'I'),
+        new Book(400, 'AngularJS', 'Susie', 'B')
     ];
-    
-    alert('book 1 = ' + this.books.length);
+
 }
 
-function Book(id, title, author, available) {
+function Book(id, title, author, status) {
     this.id = id;
     this.title = title;
     this.author = author;
-    this.available = available;
+    this.status = status;
 }
 
 LibraryController.prototype.hello = function () {
-  alert('Welcome to the Library!');  
+  alert('Welcome to the Library!');
 };
--- server.js
+++ server.js
@@ -23,28 +23,64 @@ app.listen(3000, function () {
   console.log('http://localhost:3000 is ready');
 });
 
+
+app.get('/binarysearch', function(req, res){
+   console.log('sorting');
+
+    var inputArry = [1, 6, 34, 69, 20, 18, 4, 7];
+    console.log(inputArry);
+    var index = binarysearch(inputArry, 18);
+    console.log(index);
+
+   res.redirect('/index.html');
+});
+
+
 app.get('/quicksort', function(req, res){
    console.log('sorting');
-   
+
     var inputArry = [1, 6, 34, 69, 20, 18, 4, 7];
     console.log(inputArry);
     quickSort(inputArry, 0, inputArry.length-1);
     console.log(inputArry);
- 
+
    res.redirect('/index.html');
 });
 
 
 app.get('/bubblesort', function(req, res){
    console.log('bubble sorting');
-   
+
     var inputArry = [1, 6, 34, 69, 20, 18, 4, 7];
     console.log(inputArry);
     bubbleSort(inputArry);
     console.log(inputArry);
- 
+
    res.redirect('/index.html');
 });
+
+/** DB CONNECTION LOGIC **/
+var mariasql = require('mysql'); //currently using the mysql binding since can't get the mariasql to work properly
+var connection = mariasql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'PenguinMariaDB',
+    database: 'library'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM librarian', function(err, rows, field){
+    if(!err){
+        console.log('rows: ' + rows);
+    } else {
+        console.log('error');
+    }
+});
+
+connection.end();
+
+/** RANDOM CODE SNIPPETS **/
 
 function bubbleSort(inputArry){
     for(var i = 0; i < inputArry.length - 1; i++){
@@ -56,62 +92,58 @@ function bubbleSort(inputArry){
 
 
 function quickSort(inputArry, lowerIndex, higherIndex){
-    
+
     console.log(inputArry);
-    
-    var middleIndex = inputArry.sort(lowerIndex, higherIndex);
+
+    var middleIndex = sort(inputArry, lowerIndex, higherIndex);
     console.log(middleIndex);
-    
+
     //sort the left side recursively
     if(lowerIndex < middleIndex - 1){
         quickSort(inputArry, lowerIndex, middleIndex - 1);
     }
-    
+
     if(middleIndex < higherIndex){
         quickSort(inputArry, middleIndex, higherIndex);
     }
 }
 
 
-function sort(lowerIndex, higherIndex){
-            
+function sort(inputArry, lowerIndex, higherIndex){
+
     //first, find the pivot value
-    var pivotIndex = lowerIndex + ((higherIndex - lowerIndex) / 2);
-            
+    var pivotIndex = Math.round(lowerIndex + ((higherIndex - lowerIndex) / 2));
+
     var i = lowerIndex;
     var j = higherIndex;
-    
-    var pivotValue = this.inputArr[pivotIndex];
-    
-    console.log('lowerIndex = ' + lowerIndex + ', higherIndex = ' + higherIndex);
-    console.log('pivotValue = ' + pivotValue);   
 
-    while (i <= j){    
+    var pivotValue = inputArry[pivotIndex];
+
+    while (i <= j){
         //search in the left side of the pivot, and look for the first greater value compare to pivot
-        while(this.inputArr[i] < pivotValue){
+        while(inputArry[i] < pivotValue){
             i++;
         }
-    
+
         //search in the right side of the pivot, and look for the first greater value compare to pivot
-        while(this.inputArr[j] > pivotValue){
+        while(inputArry[j] > pivotValue){
             j--;
         }
 
         //if lowerIndex is still smaller than higherIndex (meaning the sort hasn't complete), swap values and move the indexes forward
         if(i <= j){
-            this.inputArr.exchangeValue(i, j);
+            exchangeValue(inputArry, i, j);
             i++;
             j--;
         }
     }
-    
-    return i;
-    
-}
 
+    return i;
+
+}
 
 function exchangeValue(inputArr, index1, index2){
     var tempValue = inputArr[index1];
-    this.inputArr[index1] = this.inputArr[index2];
-    this.inputArr[index2] = tempValue;      
-}    
+    inputArr[index1] = inputArr[index2];
+    inputArr[index2] = tempValue;
+}
